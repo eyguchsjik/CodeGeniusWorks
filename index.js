8d5c5f0bcdeee46cd057869cbb674e983e89960b
eyguchsjik <dillonmsipsg@hotmail.com>
2024-05-10 06:31:06 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const randomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+  return maxProfit;
+}
