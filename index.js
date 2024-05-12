d47435cc66575aa81bfdb3a73311adbb19033ec8
eyguchsjik <dillonmsipsg@hotmail.com>
2024-05-13 06:15:28 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function combinationSum(candidates, target) {
+  const result = [];
+  backtrack(0, [], 0);
+  return result;
+  function backtrack(start, current, sum) {
+    if (sum === target) {
+      result.push([...current]);
+      return;
+    }
+    if (sum > target || start === candidates.length) return;
+    for (let i = start; i < candidates.length; i++) {
+      current.push(candidates[i]);
+      backtrack(i, current, sum + candidates[i]);
+      current.pop();
+    }
   }
-  return n * factorialRecursive(n - 1);
-};
+}
