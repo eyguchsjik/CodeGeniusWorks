ca32dbd0fdde9e0af9a1708a63a2948a14390935
eyguchsjik <dillonmsipsg@hotmail.com>
2024-05-15 11:13:11 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function pathSum(root, sum) {
-  if (!root) return false;
-  if (!root.left && !root.right && root.val === sum) return true;
-  return (
-    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
-  );
-}
+const fibonacciRecursive = (n) => {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+};
