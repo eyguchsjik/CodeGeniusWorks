05aaedfefbb294e532238b29de89bdfe7bc710a5
eyguchsjik <dillonmsipsg@hotmail.com>
2024-05-18 00:24:21 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("world");
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+  return maxProfit;
+}
